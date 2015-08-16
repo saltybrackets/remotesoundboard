86ba4572e629517e2c8a4eb45b12a3b6e1429ced
zachwhite <zallenwhite@gmail.com>
2015-08-15 19:51:39 -0700

    Database creation test. Type constraint test for channel insertion.

---
+++ SoundboardService.Tests/Models/ChannelTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.Entity.Validation;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Soundboard.Service.Models;
+using Soundboard.Service.Models.Sounds;
+
+
+namespace SoundboardService.Tests
+{
+	[TestClass]
+	public class ChannelTests
+	{
+
+		[TestClass]
+		public class TypeConstraints
+		{
+			[TestMethod]
+			[ExpectedException(typeof(DbEntityValidationException))]
+			public void DisallowNullNames()
+			{
+				using (SoundboardServiceContext context =
+					new SoundboardServiceContext())
+				{
+					Channel nullNameChannel = new Channel()
+					{
+						Name = null
+					};
+					context.Channels.Add(nullNameChannel);
+					context.SaveChanges();
+				}
+			}
+
+		}
+		
+	}
+}
---
+++ SoundboardService.Tests/Models/SoundboardServiceContextTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Soundboard.Service.Models;
+using Soundboard.Service.Models.Sounds;
+
+
+namespace SoundboardService.Tests
+{
+	[TestClass]
+	public class SoundboardServiceContextTests
+	{
+
+		[TestClass]
+		public class DatabaseInitialization
+		{
+			[TestMethod]
+			public void DropCreateNewDatabase()
+			{
+				using (SoundboardServiceContext context = 
+					new SoundboardServiceContext(
+						SoundboardServiceContext.DBCreationMethod.DropCreateAlways))
+				{
+					Channel globalChannel = context.Channels
+						.FirstOrDefault(channel => channel.Name == "Global");
+					Assert.IsNotNull(globalChannel);
+				}
+			}
+
+		}
+		
+	}
+}
